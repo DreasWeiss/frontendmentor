154d0ee55a5de0adb5cc799b26884412a702aae7
DreasWeiss <dreasweiss@gmail.com>
2023-09-13 14:17:49 +0300

    QR code component - done

--- src/components/tasks/qrCodeComponent/QrCodeComponent.js
+++ src/components/tasks/qrCodeComponent/QrCodeComponent.js
@@ -3,7 +3,21 @@ import './qrCodeComponent.scss';
 
 const QrCodeComponent = () => {
     return (
-        <h1>Hello World</h1>
+        <div className='qrCodeComponent'>
+            <div className="container">
+                <div className="qrcode">
+                    <div className="qrcode-items">
+                        <div className="qrcode-items-item">
+                            <img src="./images/image-qr-code.png" alt="qrcode" />
+                        </div>
+                        <div className="qrcode-items-item">
+                            <p className='title'>Improve your front-end skills by building projects</p>
+                            <p className='description'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
