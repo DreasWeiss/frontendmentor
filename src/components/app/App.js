e3ba94108e586b5b9b729a2250048b54340917d2
DreasWeiss <dreasweiss@gmail.com>
2023-09-13 12:52:26 +0300

    Starter

---
+++ src/components/app/App.js
@@ -0,0 +1,12 @@
+import QrCodeComponent from '../tasks/qrCodeComponent/QrCodeComponent';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <QrCodeComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/tasks/qrCodeComponent/QrCodeComponent.js
@@ -0,0 +1,10 @@
+import './qrCodeComponent.scss';
+
+
+const QrCodeComponent = () => {
+    return (
+        <h1>Hello World</h1>
+    )
+}
+
+export default QrCodeComponent;
